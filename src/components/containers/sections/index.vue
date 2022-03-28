97117d62081e5e591947b705bf85cd5b5830eb51
Alexis Guzman <alexis12989x@gmail.com>
2022-03-28 10:58:54 -0600

    feat: export all sections components

---
+++ src/components/containers/sections/index.vue
@@ -0,0 +1,5 @@
+<script>
+export { default as BannerSection } from "./BannerSection.vue";
+export { default as CategoriesSection } from "./CategoriesSection.vue";
+export { default as CustomersFavoritesSection } from "./CustomersFavoritesSection.vue";
+</script>
