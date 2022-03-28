8240fca335059efe8eeafc3edd0375135950d565
Alexis Guzman <alexis12989x@gmail.com>
2022-03-28 10:55:21 -0600

    feat: import header, footer and base styles

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { RouterView } from "vue-router";
+import { Header, FooterContainer } from "@/components/containers/index.vue";
+</script>
+
+<template>
+  <Header />
+
+  <RouterView />
+
+  <FooterContainer />
+</template>
+
+<style lang="scss">
+@use "@/assets/styles/base/reset";
+@use "@/assets/styles/base/typography";
+@use "@/assets/styles/base/global";
+</style>
