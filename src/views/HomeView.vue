51a62b206c436247d08076be9d5f98b8caca48b8
Alexis Guzman <alexis12989x@gmail.com>
2022-03-28 11:00:28 -0600

    feat: import banner, categories and customer favorites components

---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import {
+  BannerSection,
+  CategoriesSection,
+  CustomersFavoritesSection,
+} from "@/components/containers/sections/index.vue";
+</script>
+
+<template>
+  <BannerSection />
+  <CategoriesSection />
+  <CustomersFavoritesSection />
+</template>
