461deeedce3cc7f8ce7643c38746d45f50a22d72
Alexis Guzman <alexis12989x@gmail.com>
2022-03-28 10:57:51 -0600

    feat: export all links components

---
+++ src/components/containers/links/index.vue
@@ -0,0 +1,6 @@
+<script>
+export { default as CollectionsLinks } from "./CollectionsLinks.vue";
+export { default as FooterLinks } from "./FooterLinks.vue";
+export { default as NavLinks } from "./NavLinks.vue";
+export { default as TermsLinks } from "./TermsLinks.vue";
+</script>
