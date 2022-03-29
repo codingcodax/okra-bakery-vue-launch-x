1110dd2d01650e53a3d6ced7c94f363e3abe978e
Alexis Guzman <alexis12989x@gmail.com>
2022-03-29 16:57:44 -0600

    feat: export product header section component

--- src/components/containers/sections/index.vue
+++ src/components/containers/sections/index.vue
@@ -3,4 +3,5 @@ export { default as AllProductsSection } from "./AllProductsSection.vue";
 export { default as BannerSection } from "./BannerSection.vue";
 export { default as CategoriesSection } from "./CategoriesSection.vue";
 export { default as CustomersFavoritesSection } from "./CustomersFavoritesSection.vue";
+export { default as ProductHeaderSection } from "./ProductHeaderSection.vue";
 </script>
