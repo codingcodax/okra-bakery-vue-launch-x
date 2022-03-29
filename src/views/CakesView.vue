162ae544c2eaf8268fc2bc78de2ee22190f8a2d2
Alexis Guzman <alexis12989x@gmail.com>
2022-03-29 16:58:18 -0600

    feat: setup cakes page

--- src/views/CakesView.vue
+++ src/views/CakesView.vue
@@ -1,3 +1,22 @@
+<script setup>
+import {
+  ProductHeaderSection,
+  AllProductsSection,
+} from "@/components/containers/sections/index.vue";
+</script>
+
 <template>
-  <p>Cakes page component</p>
+  <ProductHeaderSection
+    src="/src/assets/img/assortment-pieces-cake.png"
+    alt="assortment cake in pieces"
+  >
+    <template #title>Cakes</template>
+    <template #description
+      >From Chocolate Cakes, to Fruity Cakes, to cheesecakes and so many more,
+      there is a cake for any celebration and occasion. Why not personalize a
+      special birthday message on your chosen cake to make the celebration extra
+      special.</template
+    >
+  </ProductHeaderSection>
+  <AllProductsSection />
 </template>
